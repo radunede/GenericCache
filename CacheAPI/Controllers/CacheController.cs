91d61047c55c9d080ce10dd37e948c24210a6103
Radu <radupnedelcu@gmail.com>
2020-11-16 13:29:52 +0000

    Added API Gateway

---
+++ CacheAPI/Controllers/CacheController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cache;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CacheAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CacheController : ControllerBase
+    {
+
+
+        private readonly ILogger<CacheController> _logger;
+        private readonly ICache _cache;
+
+        public CacheController(ILogger<CacheController> logger, ICache cache)
+        {
+            _logger = logger;
+            _cache = cache;
+        }
+
+        [HttpGet("{key}")]
+        public object Get(string key)
+        {
+            return _cache.Get(key);
+        }   
+    }
+}
